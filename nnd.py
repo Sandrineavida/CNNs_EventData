9971fb39fcdfd35193fc19424a7e1820916be5b1
Avida <sandrineavida@163.com>
2024-12-08 04:31:01 +0100

    FLOP test in nnd

--- nnd.py
+++ nnd.py
@@ -1,5 +1,41 @@
 import torch
 import torch.nn as nn
+from fvcore.nn import FlopCountAnalysis, flop_count_table
+import psutil
+import os
+from models.cnn_lenet import CNNLeNet, CNNwithSkip
+from models.separable_convolution import SeparableConv_LeNet
+
+model = CNNLeNet(num_classes=1, quantised=False)
+inputs = torch.randn(1, 2, 32, 32)
+flop_analysis = FlopCountAnalysis(model, inputs)
+print("FLOP for CNNLeNet with 1 input sample of size (32,32) on a Binary Classification task: ")
+print(flop_count_table(flop_analysis))
+
+# # 检查当前进程的内存使用情况
+# process = psutil.Process(os.getpid())
+# start_mem = process.memory_info().rss  # 物理内存占用（RSS）
+# output = model(inputs) # 推理
+# end_mem = process.memory_info().rss
+# print(f"Memory usage during inference: {(end_mem - start_mem) :.2f} B")
+
+
+print("\n")
+model = CNNwithSkip(num_classes=1, quantised=True)
+inputs = torch.randn(1, 2, 32, 32)
+flop_analysis = FlopCountAnalysis(model, inputs)
+print("FLOP for CNNwithSkip with 1 input sample of size (32,32) on a Binary Classification task: ")
+print(flop_count_table(flop_analysis))
+
+print("\n")
+model = SeparableConv_LeNet(num_classes=1, quantised=False)
+inputs = torch.randn(1, 2, 32, 32)
+flop_analysis = FlopCountAnalysis(model, inputs)
+print("FLOP for SeparableConv_LeNet with 1 input sample of size (32,32) on a Binary Classification task: ")
+print(flop_count_table(flop_analysis))
+
+
+
 
 # 定义一个 AdaptiveAvgPool2d 层
 pool = nn.AdaptiveAvgPool2d(1)
